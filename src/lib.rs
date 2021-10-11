328a0efe8e44e22b4333373061a9ae5aa367c17f
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2021-10-11 23:54:52 +0200

    Get count of arguments entered until now

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use cpython::{Python, PyResult, py_module_initializer, py_fn};
+use regex::Regex;
 
 fn gen_init(variables_str: &str) -> String {
     let tab = "    ";
@@ -6,10 +7,26 @@ fn gen_init(variables_str: &str) -> String {
     let mut res = String::new();
     for var in variables {
         let var = var.trim();
-        let line = format!("{}self._{} = {}\n", tab, var, var);
+        let line = format!("{}self._{} = {}\n{}", tab, var, var, tab);
         res.push_str(&line);
     }
     res
+}
+
+fn get_last_read_argument(line: &str) -> i32 {
+    let regexp = Regex::new(r"argv\[(\d+)\]").unwrap();
+    let mut caps = regexp.captures_iter(line);
+
+    let num = caps.next();
+    let nb_args_upto_this_line = match num {
+        Some(i) => i[1].parse::<i32>().unwrap(),
+        _ => 0,
+    };
+    nb_args_upto_this_line + 1
+}
+
+fn get_last_read_argument_py(_: Python, line: &str) -> PyResult<i32> {
+    Ok(get_last_read_argument(line))
 }
 
 fn gen_init_py(_:Python, variables_str: &str) -> PyResult<String> {
@@ -19,5 +36,6 @@ fn gen_init_py(_:Python, variables_str: &str) -> PyResult<String> {
 py_module_initializer!(rustsnippetsutils, |py, m| {
     m.add(py, "__doc__", "Module written in rust for use in inline-python code snippets")?;
     m.add(py, "gen_init", py_fn!(py, gen_init_py(variables_str: &str)))?;
+    m.add(py, "get_last_read_argument", py_fn!(py, get_last_read_argument_py(line: &str)))?;
     Ok(())
 });
